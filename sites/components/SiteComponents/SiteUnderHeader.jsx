c69228bc9c6f4fa4c16eef74ebd9ff52f8b9da9d
KenooG <seba123.2003@wp.pl>
2023-08-21 15:45:17 +0200

    Skonczony overlay bez podsekcji (bez backendu)

--- sites/components/SiteComponents/SiteUnderHeader.jsx
+++ sites/components/SiteComponents/SiteUnderHeader.jsx
@@ -7,10 +7,35 @@ const SiteUnderHeader = () => {
     return (
         <>
            <div className="Main_UnderHeader">
-               <div className="left"></div>
-               <fiv className="right"></fiv>
-               
-               
+               <div className="left">
+                   <div className="leftm">
+                   <a href="" className="transfer">
+                       <div className="btn btn1">
+                           <p className="text">Challanges</p>
+                       </div>
+                   </a><a href="" className="transfer">
+                   <div className="btn">
+                       <p className="text">Goals</p>
+                   </div>
+               </a><a href="" className="transfer">
+                   <div className="btn">
+                       <p className="text">Earnings</p>
+                   </div>
+               </a>
+                   <a href="" className="transfer">
+                       <div className="btn">
+                           <p className="text">Total Money <br/> Saved </p>
+                       </div>
+                   </a>
+                   </div>
+
+
+               </div>
+               <div className="right">
+              <div className="rightMain">
+
+              </div>
+               </div>
            </div>
 
 
