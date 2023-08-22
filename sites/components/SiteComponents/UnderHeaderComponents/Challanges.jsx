3f07ea995786ca5157d40c77976dd7261fc96659
KenooG <seba123.2003@wp.pl>
2023-08-22 21:06:32 +0200

    stylowanie lewej kolumny bez funkcjonalonsci

--- sites/components/SiteComponents/UnderHeaderComponents/Challanges.jsx
+++ sites/components/SiteComponents/UnderHeaderComponents/Challanges.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import CurrentDate from './CurrentDate';
-
+import RandomQuote from "./RandomQuote.jsx";
 
 
 
 const Challanges = () => {
 
 
+
+
         return (
             <>
                 <div className="Challange_Des">
-                    <p className="SavingGoal1">You are saving for : Mercedes</p>
+                    <p className="SavingGoal1">You are saving for : Dream Car</p>
                     <div className="SavingGoal2">
                         <p className="SG">Progress done : </p>
                         <p className="SG2">20%</p>
@@ -18,6 +20,13 @@ const Challanges = () => {
                 </div>
                 <div className="ChallangeDown">
                     <div className="DownLeft">
+                        <div className="up">
+                            <p><RandomQuote/></p>
+                        </div>
+
+                        <div className="down">
+                            <div className="downleft">
+
                         <div className="date">
                             <CurrentDate />
                         </div>
@@ -28,12 +37,20 @@ const Challanges = () => {
                                 <span className="dollar-sign">$</span>
                             </div>
                             <button type="submit">Add</button>
+                        </div>
+                        </div>
+                            <div className="downleftright">
+                                <div className="totalmoney"><p>Money saved already: </p><p className="totalmoneytext">6000$</p></div>
+                                <div className="toreach"><p>To reach your goal you need :</p> <p className="torachmoney">8000$</p></div>
 
+
+                            </div>
                         </div>
 
 
                     </div>
                     <div className="DownRight">
+
 
                     </div>
 
---
+++ sites/components/SiteComponents/UnderHeaderComponents/RandomQuote.jsx
@@ -0,0 +1,32 @@
+
+import React from 'react';
+
+const RandomQuote = () => {
+    const quotes = [
+        "\"If you would be wealthy, think of saving as well as getting.\" – Benjamin Franklin\n",
+        "\"Money is a terrible master but an excellent servant.\" – P.T. Barnum",
+        "\"The art is not in making money, but in keeping it.\" – Proverb",
+        "\"He who buys what he does not need steals from himself.\" – Swedish Proverb",
+        "\"A penny saved is a penny earned.\" – Benjamin Franklin",
+        "\"Do not save what is left after spending, but spend what is left after saving.\" – Warren Buffett",
+
+    ];
+
+
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    return (
+        <div className="quote-container">
+            <p className="quote-text">Quote of the day : {randomQuote}</p>
+        </div>
+    );
+};
+
+export default RandomQuote;
+
+
+
+
+
+
+
